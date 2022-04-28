52fe5191a073b21cc5a36c6bd0d95ae8eb4a02c3
Abigail Castro <ac2623@cornell.edu>
2022-04-28 15:07:57 -0400

    Initial Commit

---
+++ HackChallengeSP22/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackChallengeSP22
+//
+//  Created by Abigail Castro on 4/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackChallengeSP22/HackChallengeSP22App.swift
@@ -0,0 +1,17 @@
+//
+//  HackChallengeSP22App.swift
+//  HackChallengeSP22
+//
+//  Created by Abigail Castro on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct HackChallengeSP22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
